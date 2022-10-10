e1a732925874642cc4edafdb5fe38d891f1cbb24
RaikiWatanabe <k021g1324@g.neec.ac.jp>
2022-10-10 12:30:53 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,32 +1,38 @@
 #include <stdio.h>
-template<typename Type>
-
-Type Min(Type a, Type b) {
-	if (a <= b) {
-		return static_cast<Type>(a);
-	}
-	else if (a>b) {
-		return static_cast<Type>(b);
-	}
-
-	return 0;
+//一般的な賃金形態の計算
+int TimeMoney(int time, int money) {
+	return time* money;
 }
 
-template <>
-char Min(char a, char b) {
-	printf("数字以外は代入できません");
-	return 0;
+//再帰的な賃金形態の計算
+//時間が一定数以下になるまで呼び出す
+int Recursion(int time,int money) {
+	time--;
+	
+	if (time<1) {
+		return money;
+	}
+
+	return Recursion(time,money * 2 - 50);
 }
 
 int main() {
-	//int
-	printf("%d\n", Min<int>(5,8));
-	//float
-	printf("%f\n", Min<float>(8.0,5.0));
-	//double
-	printf("%lf\n", Min<double>(3.0,3.0));
-	//char
-	Min<char>('a', 'b');
+	int time = 100;				//時間
+	int money = 1072;			//一般的な賃金形態
+	int recursionMoney = 100;	//再帰的な賃金形態
+
+
+	//一般的な賃金形態と再帰的な賃金形態の比較
+	for (int i = 1; i < time; i++)
+	{
+		printf("%d時間働いた場合\n", i);
+		printf("一般的な賃金体態:%d\n", TimeMoney(i, money));
+		printf("再起的な賃金体態:%d\n\n", Recursion(i, recursionMoney));
+		if (TimeMoney(i, money) < Recursion(i, recursionMoney))
+		{
+			break;
+		}
+	}
 
 	return 0;
 }
