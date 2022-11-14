360e77d1a82e21782a77a3608ec34bd5152594eb
RaikiWatanabe <k021g1324@g.neec.ac.jp>
2022-11-14 11:02:22 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -12,32 +12,35 @@ int main() {
 
 	//時間
 	int second = 3;
-	
-	
+
+
 	//奇数か偶数か入力
 	int predict = 0;
 	printf("奇数だと思うなら1を、偶数だと思うなら２を入力してください。");
 	scanf_s("%d", &predict);
 
 	//時間を置く
-	std::function<void(int,int)>SetTimeout = [](int second,int ramdom) {Sleep(second * 1000); };
+	std::function<void(int, int)>SetTimeout = [](int second, int ramdom) {Sleep(second * 1000); };
 
 	//結果判定と出力
 	std::function<void(int)>Result = [=](int ramdom) {
+		
+		//時間を置く呼び出し
 		SetTimeout(second, ramdom);
-	//奇数の場合
-		if (predict ==1 ) {
+		
+		//奇数の場合
+		if (predict == 1) {
 			if (ramdom % 2 == 1) {
-				printf("正解です!\n%dが抽選で選ばれました",ramdom);
+				printf("正解です!\n%dが抽選で選ばれました", ramdom);
 			}
 			else {
 				printf("不正解です\n%dが抽選で選ばれました", ramdom);
 			}
 		}
-	
+
 		//偶数の場合
 		else if (predict == 2) {
-			if (ramdom % 2  == 0) {
+			if (ramdom % 2 == 0) {
 				printf("正解です!\n%dが抽選で選ばれました", ramdom);
 			}
 			else {
@@ -54,6 +57,6 @@ int main() {
 
 	//結果判定
 	Result(ramdom);
-	
+
 	return 0;
 }
